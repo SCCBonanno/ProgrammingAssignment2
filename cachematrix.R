302b75ebdf67115ed6d6b746f6713975f6c9b1d5
Sam Bonanno <80286094+SCCBonanno@users.noreply.github.com>
2021-04-14 20:14:22 -0400

    Update cachematrix.R
    Programming assignment 2 for peer review

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##makeCasheMatrix makes a matrix 'object' that can cashe its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- fuction() {x}
+  setInverse <- fuction(inverse) {inv <<- inverse} ##set the value of the mean
+  getInverse <- function() {inv} ##get the value of the mean
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function includes an if statement that allows R to either calculate the matrix inverse, or pull the existing solution from the cashe. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cashed data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv     
 }
